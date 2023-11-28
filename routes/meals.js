07062cb1aa5bd5eebb7598ca5a17f3a4d7b3535a
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-11-28 17:14:59 +0300

    set up router and routes

---
+++ routes/meals.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    getAllMeals,
+    getMeal,
+    createMeal,
+    updateMeal,
+    deleteMeal
+} = require('../controllers/meals');
+
+router.route('/').post(createMeal).get(getAllMeals);
+router.route(':id').get(getMeal).delete(deleteMeal).patch(updateMeal);
+
+module.exports = router;
