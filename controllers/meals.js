88b9016a3f54ef35add97e03335275fb289320c1
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-11-28 17:03:31 +0300

    add basic set up of controllers

---
+++ controllers/meals.js
@@ -0,0 +1,28 @@
+const getAllMeals = async (req, res) => {
+    res.send('register user')
+};
+
+const getMeal = async (req, res) => {
+    res.send('login user')
+};
+
+const createMeal = async (req, res) => {
+    res.send('create meal')
+};
+
+const updateMeal = async (req, res) => {
+    res.send('update meal')
+};
+
+const deleteMeal = async (req, res) => {
+    res.send('delete meal')
+};
+
+
+module.exports = {
+    getAllMeals,
+    getMeal,
+    createMeal,
+    updateMeal,
+    deleteMeal
+};
