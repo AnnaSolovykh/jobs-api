877845fd1d9e855eeeb3f01cab5ad46032f30b99
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-11-30 10:48:47 +0300

    add login functionality to the controller

--- models/User.js
+++ models/User.js
@@ -28,7 +28,7 @@ const UserSchema = new mongoose.Schema({
 UserSchema.pre('save', async function () {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-})
+});
 
 UserSchema.methods.createJWT = function () {
     return jwt
@@ -40,6 +40,12 @@ UserSchema.methods.createJWT = function () {
             process.env.JWT_SECRET, 
             {
                 expiresIn: process.env.JWT_LIFETIME
-            })};
+            })
+};
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+};
 
 module.exports = mongoose.model('User', UserSchema);
