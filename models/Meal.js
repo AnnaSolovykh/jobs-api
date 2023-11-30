8e03a7ed18050b40ea22838e1860b7eabd852619
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-11-30 12:01:47 +0300

    set up meal model

---
+++ models/Meal.js
@@ -0,0 +1,36 @@
+
+const mongoose = require('mongoose');
+
+const MealSchema = new mongoose.Schema({
+    title: {
+        type: String, 
+        required: [true, 'name must be provided'],
+        trim: true,
+        maxlength: [40, 'the name of the meal should not be longer than 40 characters']
+    },
+    type: {
+        type: String,
+        enum: [
+            'breakfast',
+            'snack',
+            'lunch',
+            'dinner',
+        ],
+        required: [true, 'type of the meal must be provided'],
+    },
+    isFavorite: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Please provide a user']
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Meal', MealSchema);
