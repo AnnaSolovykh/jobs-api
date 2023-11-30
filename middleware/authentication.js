a27ab346d77bd1a64510be3d330222fb2c2b8c2a
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-11-30 12:29:58 +0300

    fix typo in req.headers.authorization

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const { UnauthenticatedError } = require('../errors');
 
 const auth = async (req, res, next) => {
-    const authHeader = req.headers.authrozation;
+    const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
         throw new UnauthenticatedError('Authentication invalid')
     }
@@ -11,7 +11,7 @@ const auth = async (req, res, next) => {
     try {
         const payload = jwt.verify(token, process.env.JWT_SECRET);
         //attach the user to the meals routes
-        req.user = { userId: payload.userId, name: payload.name }
+        req.user = { userId: payload.userId, name: payload.name };
 
         //alternative code as an optional replacement of the previous line:
         //const user = User.findById(payload.id).select('-password');
